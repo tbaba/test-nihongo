e56f70c7754c498eb660e5de677cd392c85c3852
Tatsuro Baba <harakirisoul@gmail.com>
2022-05-18 13:22:38 +0900

    Sample code

--- テスト.py
+++ テスト.py
@@ -1,1 +1,25 @@
-テスト
+# -*- coding: utf-8 -*-
+ 
+a="Hello"
+b="world"
+ 
+#文字の連結
+c=a+b
+print( "%s+%s=%s" % ( a,b,c ) )
+ 
+#文字列の繰り返し
+f=a*10 print( f )
+ 
+#フォーマット文字列で整形
+d=format( '%s %s' % (a,b) )
+print( d )
+ 
+#置換
+g=b.replace('or','OR')
+print( g )
+ 
+#文字列両端のスペース、タブ文字、改行を削除
+h=' hello \n\n\n'
+print( h )
+i=h.strip()
+print( i )
